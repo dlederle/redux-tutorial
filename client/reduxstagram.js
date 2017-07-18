9acaaccce32258b6db48792e4344f17e6d3b465a
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2017-07-18 15:23:53 -0400

    Setting up initial components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//Import css
+import css from './styles/style.styl';
+
+import Main from './component/Main.js';
+
+render(<Main/>, document.getElementById('root'));
