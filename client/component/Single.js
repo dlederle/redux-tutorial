e19929081181c641fbc000fd52fbb2db37db6665
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2017-07-19 17:07:08 -0400

    Displaying comments

--- client/component/Single.js
+++ client/component/Single.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Photo from './Photo.js';
+import Comments from './Comments.js';
 
 const Single = React.createClass({
   render() {
+    // postID is from the URL string, provided through react-router
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-      I'm the single
+        <Photo i={i} post={post} {...this.props } />
+        <Comments postComments={postComments} />
       </div>
     )
   }
